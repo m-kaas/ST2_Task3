da84285067219a16ecc1dc6e237179deea006cd7
Liubou Sakalouskaya <ly2708sokol@gmail.com>
2019-06-15 12:51:16 +0300

    Initial Commit

---
+++ ImagesTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagesTable
+//
+//  Created by Liubou Sakalouskaya on 6/15/19.
+//  Copyright © 2019 Liubou Sakalouskaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagesTable/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ImagesTable
+//
+//  Created by Liubou Sakalouskaya on 6/15/19.
+//  Copyright © 2019 Liubou Sakalouskaya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
